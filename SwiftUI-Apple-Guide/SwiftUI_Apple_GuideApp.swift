ecb0175d4ddd62d725acfbb306200e69c53c6a7a
Karthi Rasu <karthirasu07@gmail.com>
2024-05-07 22:12:10 +0530

    Initial Commit

---
+++ SwiftUI-Apple-Guide/SwiftUI_Apple_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_GuideApp.swift
+//  SwiftUI-Apple-Guide
+//
+//  Created by Karthi Rasu on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
